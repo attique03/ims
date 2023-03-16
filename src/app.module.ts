294ecf5a1fde1866ace188543dd4e6ed22a1e319
attique03 <muhammad.attique@gigalabs.co>
2023-03-16 17:37:50 +0500

    Some Changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,6 +45,6 @@ import { AssetsModule } from './assets/assets.module';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(authMiddleware).forRoutes('user');
+    consumer.apply(authMiddleware).forRoutes('users');
   }
 }
